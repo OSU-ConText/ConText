35d2b19d577e213470294190cb2edff5e5be4b7e
Mesharp01 <54689768+Mesharp01@users.noreply.github.com>
2023-03-21 10:12:38 -0400

    worked on structure for demo 2 with new database functions

--- demoTwo.py
+++ demoTwo.py
@@ -3,9 +3,13 @@ from wonderwords import RandomSentence
 import msgGeneration
 import database
 
+#need database functions that can: 
+# query for a specific user, 
+# query for a specific conversation, 
+# show all conversations for a specific user
+
 def messageSimulation():
     sendMessage()
-    printStats()
     return
 
 def printStats(user):
@@ -16,22 +20,36 @@ def printStats(user):
 def sendMessage(userLanguage):
     #have user input the language they want to send a message in
     #create a new user
-    database.create_user
+    database.create_user()
+    #display user row in database
+    user = findUser()
+    printStats(user)
+    #create a conversation for the user
+    database.create_conversation()
+    #display user conversations in database
     findUserConversations()
+    #select a certain conversation we want to update/send a message in
     selectUserConversation()
-    #print out database row to show update worked (user was successfully created)
-    messageTranslation(userLanguage)
-    #update database with user stats
+    #send a message in messageTranslation function
+    sendMessageInConvo(userLanguage)
     #print out database row to show update worked (lang parameters were successfully updated)
+    database.get_language_columns()
     return
 
+#function that calls database backend function to return all conversations for a specific user
 def findUserConversations():
     return
 
+#function that selects the conversation we are going to send a message in
 def selectUserConversation():
     return
 
-def messageTranslation(userLanguage):
+#function that calls database backend functions to return the row for a specific user
+def findUser():
+    return
+
+#funtion that simulates sending a message
+def sendMessageInConvo(userLanguage):
     #"send" one message
     message = msgGeneration.generateMessage
     print(message)
