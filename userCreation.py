a9e5e8db80010d4a03e89b9d1c787007aa8f2816
Mesharp01 <54689768+Mesharp01@users.noreply.github.com>
2023-03-21 21:52:20 -0400

    code to send a message in a random language in a random conversation

--- userCreation.py
+++ userCreation.py
@@ -30,5 +30,25 @@ def createConversation():
     user_two = createUser()
     database.create_sent_history(user_one, user_two)
 
+#send a message, update history
+def sendMessage(sent_id, lang):
+    database.update_history(sent_id, lang)
 
+#return all conversations for a user
+def getConversations(user_id):
+    user_convos = []
+    user_convos = database.get_sent_ids(user_id)
+    return user_convos
 
+#return users preferred language
+def userLang(sent_id):
+    lang = database.get_preferred_lang(sent_id)
+    return lang
+
+#finds all of a given users conversations, chooses a random conversation to send a message in
+#sends a message in a randomly generated language
+def sendInConversation(user_id):
+    convos = getConversations(user_id)
+    rand = random.randint(1, len(convos))
+    user_generated_lang = generateMessageLanguage(user_id)
+    sendMessage(rand, user_generated_lang)
