5be9f64bd3c365683cb784b933814bf943538a73
Mesharp01 <54689768+Mesharp01@users.noreply.github.com>
2023-03-24 16:23:25 -0400

    continued working on data generation function

--- userCreation.py
+++ userCreation.py
@@ -7,26 +7,32 @@ import languages
 
 #global variable to store users and their personas (languages)
 user_personas = {}
-#dictionary to store all of a user's messages (userid, list of messages)
-all_user_messages = {}
-#dictionary to store messages in a specific conversation (convoid, list of messages)
-convo_messages = {}
 
-#creates a user and their list of languages
-def createUser():
-    user_id = database.create_user()
+def createUserPersona(user_id):
     user_langs = []
     num_languages = random.randint(1, 5)
     for i in range(num_languages):
         user_langs.insert(i, random.choice(list(languages.LANGUAGES.keys())))
     user_personas[user_id] = user_langs
-    print(user_personas)
+    #print(user_personas)
+
+
+#creates a user and their list of languages
+def createUser():
+    user_id = database.create_user()
+    createUserPersona(user_id)
     return user_id
 
 #randomly chooses a language from the users list
 def generateMessageLanguage(user_id):
-    choosenLang = random.choice(user_personas[user_id])
-    print(choosenLang)
+    createUserPersona(user_id)
+    persona = (user_personas[user_id])
+    #print(persona)
+    users_langs = user_personas[user_id]
+    choosenLang = random.choice(users_langs)
+    #print("Choosen lang: " + choosenLang)
+    #print(choosenLang)
+    return choosenLang
 
 #creates conversation between two users
 def createConversation(user_one,user_two):
@@ -34,6 +40,8 @@ def createConversation(user_one,user_two):
 
 #send a message, update history
 def sendMessage(sent_id, lang):
+    print("convo id: " + str(sent_id))
+    print("convo lang: " + lang)
     database.update_history(sent_id, lang)
 
 #return all conversations for a user
@@ -51,13 +59,15 @@ def receiverLang(sent_id):
 #sends a message in a randomly generated language
 def sendInConversation(user_id):
     convos = getConversations(user_id)
-    # rand = random.randint(1, len(convos))
-    conversation_id = convos[1]
-    print("*****")
-    print(user_id)
-    print(conversation_id)
-    print("*****")
+    #print("all convos: ")
+    #for i in range(len(convos)):
+        #print(convos[i])
+    conversation_id = random.choice(convos)
+    while conversation_id < 0:
+        conversation_id = random.choice(convos)
+    #print("convo id: " + str(conversation_id))
     user_generated_lang = generateMessageLanguage(user_id)
+    print("user id: " + str(user_id))
     sendMessage(conversation_id, user_generated_lang)
 
 #Starts multiple conversations for each user
@@ -75,10 +85,11 @@ def generateConversations():
     for i, user1 in enumerate(user_personas):
         for user2 in (list(user_personas.keys())[i+1:]):
             createConversation(user1, user2)
-            # sendInConversation(user1)
-
-    # for user in user_personas:
-    #     sendInConversation(user)
+    
+    for i in range(2):
+        for user in user_personas:
+            print("Sending a message from front end")
+            sendInConversation(user)
 
 if __name__ == '__main__':
     generateConversations()
