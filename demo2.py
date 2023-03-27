d78ca72446bdc5a859f5f560fd69b1475c8a9036
Mesharp01 <54689768+Mesharp01@users.noreply.github.com>
2023-03-27 13:59:09 -0400

    refined demo 2 flow

--- demo2.py
+++ demo2.py
@@ -31,11 +31,11 @@ translator = Translator()
 while (not_quit):
     print('\n')
     print('Enter a command')
-    print('[U] create a new user')
-    print('[C] create a new conversation')
-    print('[P] print context parameters for a conversation')
-    print('[F] find the users in a conversation')
-    print('[FC] find all conversations a user is in')
+    print('[U] Create a new user')
+    print('[C] Create a new conversation')
+    print('[P] Print context parameters for a conversation')
+    print('[F] Find the users in a conversation')
+    print('[FC] Find all conversations a user is in')
     print('[T] Send a text in a conversation')
     print('[Q] End Demo')
 
@@ -47,9 +47,9 @@ while (not_quit):
         user_id_1 = input('What is the first user_id? ')
         user_id_2 = input('What is the second user_id? ')
         database.create_sent_history(user_id_1, user_id_2)
-        print("A conversation between " + str(user_id_1) + "and " + str(user_id_2) + " has been started and added to the database")
+        print("A conversation between user " + str(user_id_1) + " and user " + str(user_id_2) + " has been started and added to the database")
     elif (user_input == 'P'):
-        sent_id = input('what sent_id would you like to gather data from\n')
+        sent_id = input('What conversation would you like to gather data from?\n')
         history = database.get_all_sent_history_info(sent_id)
         print(history)
     elif (user_input == 'F'):
@@ -62,12 +62,12 @@ while (not_quit):
         user_id = input('What user would you like to view conversations for? ')
         sent_ids = database.get_sent_ids(user_id)
         pos_convos = [ele for ele in sent_ids if ele > 0]
-        print("conversations user", user_id, " is in ",pos_convos)
+        print("conversations user", user_id, " is in: ",pos_convos)
     
     #sent typed message in specified language with sent_id entered by user
     elif(user_input == 'T'):
-        sent_id = input("\nwhat conversation would you like to send a message in\n")
-        user_input = input("Do you want to [G]enerate a random message or [T]ype a message\n")
+        sent_id = input("\nWhat conversation would you like to send a message in?\n")
+        user_input = input("Do you want to [G]enerate a random message or [T]ype a message?\n")
         if (user_input == "T"):
             text_message = input("Type the message you want to send in English\n")
         else:
@@ -79,6 +79,7 @@ while (not_quit):
         received_lang = database.get_recipient_lang(sent_id)
         received_message = translator.translate(sent_message,dest = received_lang).text
         print("\nThe translated sentence received by the other user in the conversation is:\n " + received_message + "\n")
+        print("\nThe language of the sentence the user received is: " + received_lang)
 
     elif (user_input == 'Q'):
         print('Thanks for stopping by!')
