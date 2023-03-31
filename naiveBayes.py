c0a80c028e28e27c3efb90b33454d46726889499
Mesharp01 <54689768+Mesharp01@users.noreply.github.com>
2023-03-31 16:26:38 -0400

    calculated conditional prob

--- naiveBayes.py
+++ naiveBayes.py
@@ -10,6 +10,11 @@ param_one_lang = {}
 param_two_lang = {}
 param_three_lang = {}
 
+#conditional probabilities for each parameter
+cp_param_one = {}
+cp_param_two = {}
+cp_param_three = {}
+
 # Split the dataset by class values, returns a dictionary
 def separate_by_class(dataset):
     separated = {}
@@ -59,6 +64,26 @@ def find_total_lang_instance(param_list):
         param_three_lang[param_list[2]] = 1
 
 #calculate probability of param = lang decision
+def conditional_prob():
+    total_param_one = sum(param_one_lang.values())
+    total_param_two = sum(param_two_lang.values())
+    total_param_three = sum(param_three_lang.values())
+    print(total_param_one)
+    for lang in param_one_lang:
+        if param_one_lang_is_decision.get(lang) is None:
+            cp_param_one[lang] = 0
+        else:
+            cp_param_one[lang] = param_one_lang_is_decision[lang] / total_param_one
+    for lang in param_two_lang:
+        if param_two_lang_is_decision.get(lang) is None:
+            cp_param_two[lang] = 0
+        else:
+            cp_param_two[lang] = param_two_lang_is_decision[lang] / total_param_two
+    for lang in param_three_lang:
+        if param_three_lang_is_decision.get(lang) is None:
+            cp_param_three[lang] = 0
+        else:
+            cp_param_three[lang] = param_three_lang_is_decision[lang] / total_param_three
 
 #test separating data by class (language decision)
 data = [["da", "am","da","da"],
@@ -77,6 +102,8 @@ for key in data_set:
     for list in data_set[key]:
         find_total_lang_instance(list)
 
+conditional_prob()
+
 #print(param_one_lang)
 #print(param_two_lang)
 #print(param_three_lang)
@@ -84,3 +111,7 @@ for key in data_set:
 #print(param_one_lang_is_decision)
 #print(param_two_lang_is_decision)
 #print(param_three_lang_is_decision)
+
+print(cp_param_one)
+print(cp_param_two)
+print(cp_param_three)
