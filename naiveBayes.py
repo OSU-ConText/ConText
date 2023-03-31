880e9b6de80564d5f1f113b34c3ed5a9a43d4940
Mesharp01 <54689768+Mesharp01@users.noreply.github.com>
2023-03-31 15:58:00 -0400

    continued working on counts for params and param equal lang decision

--- naiveBayes.py
+++ naiveBayes.py
@@ -1,45 +1,86 @@
-#create a dictionary of languages for each parameter and each languages count in the training data
+#dictionary for each parameter
+#counts how many times the parameter lang = decision lang
 param_one_lang_is_decision = {}
 param_two_lang_is_decision = {}
 param_three_lang_is_decision = {}
-#dictionary for number of times a lang appears in all data
-overall_langs = {}
 
+#dictionary for each parameters
+#counts how many times the parameter lang occurs
+param_one_lang = {}
+param_two_lang = {}
+param_three_lang = {}
 
-def find_freq_table(data):
-    for key in data:
-        values = data[key]
-        #dictionary with first param matching translation decision
-        if key is values[0]:
-            if param_one_lang_is_decision.get(values[0]) is not None:
-                param_one_lang_is_decision[key] += 1
-            else:
-                param_one_lang_is_decision[key] = 1
-        #dictionary with second param matching translation decision
-        if key is values[1]:
-            if param_two_lang_is_decision.get(values[1]) is not None:
-                param_two_lang_is_decision[key] += 1
-            else:
-                param_two_lang_is_decision[key] = 1
-        #dictionary with third param matching translation decision
-        if key is values[2]:
-            if param_three_lang_is_decision.get(values[2]) is not None:
-                param_three_lang_is_decision[key] += 1
-            else:
-                param_three_lang_is_decision[key] = 1
+# Split the dataset by class values, returns a dictionary
+def separate_by_class(dataset):
+    separated = {}
+    for i in range(len(dataset)):
+        list = dataset[i]
+        class_value = list[-1]
+        list.pop()
+        if (class_value not in separated):
+            separated[class_value] = []
+        separated[class_value].append(list)
+    return separated
 
-    print(param_one_lang_is_decision)
-    print(param_two_lang_is_decision)
-    print(param_three_lang_is_decision)
+#fill param = lang decision dictionaries
+def find_freq_table(decision, param_list):
+    #dictionary with first param matching translation decision
+    if decision is param_list[0]:
+        if param_one_lang_is_decision.get(decision) is not None:
+            param_one_lang_is_decision[decision] += 1
+        else:
+            param_one_lang_is_decision[decision] = 1
+    #dictionary with second param matching translation decision
+    if decision is param_list[1]:
+        if param_two_lang_is_decision.get(decision) is not None:
+            param_two_lang_is_decision[decision] += 1
+        else:
+            param_two_lang_is_decision[decision] = 1
+    #dictionary with third param matching translation decision
+    if decision is param_list[2]:
+        if param_three_lang_is_decision.get(decision) is not None:
+            param_three_lang_is_decision[decision] += 1
+        else:
+            param_three_lang_is_decision[decision] = 1
 
+#fill parameter lang count dictionaries
+def find_total_lang_instance(param_list):
+    if param_one_lang.get(param_list[0]) is not None:
+        param_one_lang[param_list[0]] += 1
+    else:
+        param_one_lang[param_list[0]] = 1
+    if param_two_lang.get(param_list[1]) is not None:
+        param_two_lang[param_list[1]] += 1
+    else:
+        param_two_lang[param_list[1]] = 1
+    if param_three_lang.get(param_list[2]) is not None:
+        param_three_lang[param_list[2]] += 1
+    else:
+        param_three_lang[param_list[2]] = 1
 
-#test separating data by class
-data = {"da": ["am","da","da"],
-        "am": ["am","am","am"],
-        "af": ["af","sn","af"],
-        "vi": ["vi","vi","vi"],
-        "ug": ["af","ug","ug"],
-        "af": ["af","af","af"]}
-find_freq_table(data)
+#calculate probability of param = lang decision
 
-#need to have nested tuples for repeating af results.
+#test separating data by class (language decision)
+data = [["da", "am","da","da"],
+["af","af","sn","af"],
+["am","am","am","am"],
+["vi","vi","vi","vi"],
+["ug","af","ug","ug"],
+["af","af","af","af"]]
+data_set = separate_by_class(data)
+#print(data_set)
+for key in data_set:
+    for list in data_set[key]:
+        find_freq_table(key, list)
+
+for key in data_set:
+    for list in data_set[key]:
+        find_total_lang_instance(list)
+
+#print(param_one_lang)
+#print(param_two_lang)
+#print(param_three_lang)
+
+#print(param_one_lang_is_decision)
+#print(param_two_lang_is_decision)
+#print(param_three_lang_is_decision)
