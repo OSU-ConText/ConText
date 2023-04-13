ebebf2da8aa69748ad711da48cd2477f85abf776
henguyen2110 <nguyen.2110@osu.edu>
2023-04-13 10:36:14 -0400

    bug fixes: error warnings and create table once

--- demo3.py
+++ demo3.py
@@ -1,3 +1,4 @@
+import time
 import streamlit as st
 import pandas as pd
 import database as db
@@ -37,8 +38,15 @@ def insert_conversation(sent_id, sender_id, sender_name, receiver_id, receiver_n
             
             VALUES ({sent_id}, {sender_id},\'{sender_name}\',{receiver_id},\'{receiver_name}\')''')
 
-if(dbh.check_table_existence("usernames", True) == False):
-    dbh.create_tables()
+db_created = dbh.check_table_existence("usernames", True)
+while (not db_created):
+    with st.spinner('Creating database...'):
+        dbh.create_tables()
+        time.sleep(2)
+        db_created = dbh.check_table_existence("usernames", True)
+
+     
+
 options = ['Create User', 'Create Conversation', 'Send Message']
 selected = st.sidebar.radio('Choose Functionality:', options)
 
@@ -55,6 +63,7 @@ if selected == 'Create User':
     if clicked:
         user_id = db.create_user()
         insert_user(st.session_state.name, user_id)
+        st.success("Created user")
     q = query_usernames()
     users = dict([(value, key) for key, value in q])
     st.markdown('All Users:')
@@ -63,79 +72,89 @@ if selected == 'Create User':
 
 if selected == 'Create Conversation':
     st.markdown(f'### Create a Conversation') 
-    col1, col2 = st.columns(2)
+    if len(users) >= 2:
+        col1, col2 = st.columns(2)
 
-    with col1:
-        user1 = st.selectbox("User 1:",users.keys(),index=0)
+        with col1:
+            user1 = st.selectbox("User 1:",users.keys(),index=0)
 
-    with col2:
-        user2 = st.selectbox( "User 2:",users.keys(),index=1)
-    
-    clicked = st.button('OK', key='ok1')
+        with col2:
+            user2 = st.selectbox( "User 2:",users.keys(),index=1)
+        
+        clicked = st.button('OK', key='ok1')
 
-    if clicked:
-        if user1 == user2:
-            st.error("Must choose two different users")
-        else:
-            ids = db.create_sent_history(users[user1], users[user2])
-            if ids == None:
-                st.success("Users already in conversation")
+        if clicked:
+            if user1 == user2:
+                st.error("Must choose two different users")
             else:
-                insert_conversation(ids[0],users[user1],user1,users[user2],user2)
-                insert_conversation(ids[1],users[user2],user2,users[user1],user1)
-                st.success("Added users to conversations")
-            q = query_conversations()   
-            c = []
-            receiver, sender = "", ""
-            for item in q:
-                if not (receiver == item[2] and sender == item[4]):
-                    sender = item[2]
-                    receiver = item[4]
-                    c.append(f"{sender} & {receiver}")
-            st.markdown('All Convos:')
-            for item in c:
-                st.markdown(f'- {item}') 
+                ids = db.create_sent_history(users[user1], users[user2])
+                if ids == None:
+                    st.success("Users already in conversation")
+                else:
+                    insert_conversation(ids[0],users[user1],user1,users[user2],user2)
+                    insert_conversation(ids[1],users[user2],user2,users[user1],user1)
+                    st.success("Added users to conversations")
+        
+        st.markdown('All Convos:')
+        q = query_conversations()   
+        c = []
+        receiver, sender = "", ""
+        for item in q:
+            if not (receiver == item[2] and sender == item[4]):
+                sender = item[2]
+                receiver = item[4]
+                c.append(f"{sender} & {receiver}")
+        for item in c:
+            st.markdown(f'- {item}') 
+
+    else:
+        st.error('Must create at least two different users')
     
 
 if selected == 'Send Message':
-    st.markdown(f'### Send a Message')                      
-    conversation = st.selectbox("Choose Conversation:",convos)
-    message_creation = st.radio("How would you like the message to be created?", ["Generate message", "Type message"],
-                                index=1)#,on_change=st.experimental_rerun)
-    if (message_creation == "Type message"):
-        text_message = st.text_input("Message (in English)")
+    st.markdown(f'### Send a Message')  
+    if len(convos) > 0:                    
+        conversation = st.selectbox("Choose Conversation:",convos)
+        message_creation = st.radio("How would you like the message to be created?", ["Generate message", "Type message"],
+                                    index=1)#,on_change=st.experimental_rerun)
+        if (message_creation == "Type message"):
+            text_message = st.text_input("Message (in English)")
+        else:
+            text_message = mc.generateMessage()
+        lang = st.selectbox("What language would you like to send the message in",languages.LANGUAGES.values())
+        # Every form must have a submit button.
+        submitted = st.button("Submit")
+
+        if submitted:
+            translator = Translator()
+            sent_message = translator.translate(text_message,dest=lang).text
+            st.markdown(f'Your message: **{sent_message}**')
+            str = re.split('=|,',conversation)
+            sender = str[1].strip()
+            receiver = str[3].strip()
+            sent_id = get_sent_id(sender,receiver)[0]
+            abbr = {i for i in languages.LANGUAGES if languages.LANGUAGES[i]==lang}.pop()
+
+            db.update_history(sent_id,abbr)
+            
+            
+            received_lang = db.get_recipient_lang(sent_id)
+            received_message = translator.translate(sent_message,dest = received_lang).text
+            st.success(f"Sent {sender}'s message to {receiver}")
+            st.markdown(f'{receiver} received the message in: **{languages.LANGUAGES.get(received_lang)}**' )
+            st.markdown(f'The message {receiver} received is: **{received_message}**')
+            st.markdown('### Why?')
+            st.markdown(f'We used the data from the messages {receiver} has sent to decide!')
+
+            recipient_history_id = db.get_all_sent_history_info(sent_id).get("recipient_history_id")
+            params = db.get_params(recipient_history_id)
+            all_messages_lang = languages.LANGUAGES.get(params.get('all_messages_lang'))
+            conv_messages_lang = languages.LANGUAGES.get(params.get('conv_messages_lang'))
+            last_message_lang = languages.LANGUAGES.get(params.get('last_message_lang'))
+            st.markdown(f'Top language of the messages that {receiver} has sent in all conversations: **{all_messages_lang}**')
+            st.markdown(f'Top language of the messages that {receiver} has sent in this conversation with {sender}: **{conv_messages_lang}**')
+            st.markdown(f'Language of the last message that {receiver} has sent in this conversation with {sender}: **{last_message_lang}**')
+
+
     else:
-        text_message = mc.generateMessage()
-    lang = st.selectbox("What language would you like to send the message in",languages.LANGUAGES.values())
-    # Every form must have a submit button.
-    submitted = st.button("Submit")
-    if submitted:
-        translator = Translator()
-        sent_message = translator.translate(text_message,dest=lang).text
-        st.markdown(f'Your message: **{sent_message}**')
-        str = re.split('=|,',conversation)
-        sender = str[1].strip()
-        receiver = str[3].strip()
-        sent_id = get_sent_id(sender,receiver)[0]
-        abbr = {i for i in languages.LANGUAGES if languages.LANGUAGES[i]==lang}.pop()
-
-        db.update_history(sent_id,abbr)
-        
-        
-        received_lang = db.get_recipient_lang(sent_id)
-        received_message = translator.translate(sent_message,dest = received_lang).text
-        st.markdown(f'{receiver} received the message in: **{received_lang}**' )
-        st.markdown(f'The message {receiver} received is: **{received_message}**')
-        st.markdown('### Why?')
-        st.markdown(f'We used the data from the messages {receiver} has sent to decide!')
-
-        recipient_history_id = db.get_all_sent_history_info(sent_id).get("recipient_history_id")
-        params = db.get_params(recipient_history_id)
-        all_messages_lang = params.get('all_messages_lang')
-        conv_messages_lang = params.get('conv_messages_lang')
-        last_message_lang = params.get('last_message_lang')
-        st.markdown(f'Top language of the messages that {receiver} has sent in all conversations: **{all_messages_lang}**')
-        st.markdown(f'Top language of the messages that {receiver} has sent in this conversation with {sender}: **{conv_messages_lang}**')
-        st.markdown(f'Language of the last message that {receiver} has sent in this conversation with {sender}: **{last_message_lang}**')
-
-
+        st.error('Must create at least one conversation')
